3770b96b2ce77c210ba072426cc7d085cfc42eb0
NikkitaTorres <nikkitatorres@yahoo.com>
2024-01-17 09:31:11 -0800

    finish project setup

---
+++ Coin-Combinations.Tests/ModelTests/CombinationsTests.cs
@@ -0,0 +1,4 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Coin-Combinations.Models;
+
+namespace Coin-Combinations.Tests
---
+++ Models/Combinations.cs
@@ -0,0 +1,4 @@
+namespace Coin-Combinations.Models
+{
+  
+}
